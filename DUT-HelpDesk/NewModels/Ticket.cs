b4f16a9f2adddd41f08bedc01149a3127e8ae79f
Ayrton Mulqueeny <ST10083575@vcconnect.edu.za>
2023-08-28 13:36:13 +0000

    Added NewModels

---
+++ DUT-HelpDesk/NewModels/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DUT_HelpDesk.NewModels;
+
+public partial class Ticket
+{
+    public int TicketId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? TechnicianId { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? QueryBody { get; set; }
+
+    public string? Priority { get; set; }
+
+    public DateTime? DateCreated { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? DateClosed { get; set; }
+
+    public virtual ICollection<Attachment> Attachments { get; set; } = new List<Attachment>();
+
+    public virtual ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+
+    public virtual ICollection<Reply> Replies { get; set; } = new List<Reply>();
+
+    public virtual Technician? Technician { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ DUT-HelpDesk/NewModels/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DUT_HelpDesk.NewModels;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FbId { get; set; } = null!;
+
+    public string? Type { get; set; }
+
+    public virtual ICollection<Technician> Technicians { get; set; } = new List<Technician>();
+
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
