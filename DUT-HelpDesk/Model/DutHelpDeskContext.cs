127c0582d2afcff3d15f3bd089cf4c3ca6dde8bb
EvanCode20 <evan.teague11@gmail.com>
2023-08-28 14:45:34 +0200

    progess on create ticket

--- DUT-HelpDesk/Model/DutHelpDeskContext.cs
+++ DUT-HelpDesk/Model/DutHelpDeskContext.cs
@@ -27,7 +27,7 @@ public partial class DutHelpDeskContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:duthelpdeskserver.database.windows.net,1433;Initial Catalog=Dut-HelpDesk;Persist Security Info=False;User ID=admin0;Password=admin12!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:bitdevs.database.windows.net,1433;Initial Catalog=bitdevsdb;Persist Security Info=False;User ID=BitDevs;Password=Codebit7;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
