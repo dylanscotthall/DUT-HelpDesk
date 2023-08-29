0845d20e4c3e74dd5d60e78f39f35a7105f55883
EvanCode20 <evan.teague11@gmail.com>
2023-08-29 14:24:52 +0200

    created create ticket page

--- DUT-HelpDesk/Controllers/HomeController.cs
+++ DUT-HelpDesk/Controllers/HomeController.cs
@@ -3,12 +3,13 @@ using DUT_HelpDesk.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 
 namespace DUT_HelpDesk.Controllers
 {
     public class HomeController : Controller
     {
-        private DutHelpdeskdbContext db = new DutHelpdeskdbContext();
+        private NewModels.DutHelpdeskdbContext db = new NewModels.DutHelpdeskdbContext();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -36,7 +37,7 @@ namespace DUT_HelpDesk.Controllers
             {
                 return StatusCode(400);
             }
-            Models.Ticket ticket = db.Tickets.Find(id);
+            NewModels.Ticket ticket = db.Tickets.Find(id);
             if (ticket == null)
             {
                 return StatusCode(400);
@@ -49,11 +50,13 @@ namespace DUT_HelpDesk.Controllers
             return View();
         }
 
-        public async Task<IActionResult> CreateTicket(Models.Ticket ticket)
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(NewModels.TicketViewModel ticket)
         {
             if(ticket != null)
             {
-                await db.Tickets.AddAsync(ticket);
+                //await db.Tickets.AddAsync(ticket);
+                Console.WriteLine($"File content type: {ticket.File.ContentType}");
                 return RedirectToAction("UserTicket");
             }
             else
@@ -62,6 +65,44 @@ namespace DUT_HelpDesk.Controllers
             }
 
         }
+
+        [HttpPost]
+        public async Task<ActionResult> AddAttachment(IFormFile file)
+        {
+            Console.WriteLine($"File content type: {file.ContentType}");
+            if (file != null && file.Length > 0)
+            {
+
+                using (var stream = new MemoryStream())
+                {
+                    await file.CopyToAsync(stream);
+
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        string content = await streamReader.ReadToEndAsync();
+
+
+
+                    }
+                    //var uploadedFile = new NewModels.File
+                    //{
+                       // FileName = file.FileName,
+                       // FileContent = stream.ToArray(),
+
+                    //};
+
+                    //NewModels.TestFiles2Context db = new NewModels.TestFiles2Context();
+                    //uploadedFile.FileId = 8;
+                    //db.Files.Add(uploadedFile);
+                    //await db.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("CreateTicket", "Home");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ DUT-HelpDesk/NewModels/TicketViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DUT_HelpDesk.NewModels
+{
+    public class TicketViewModel
+    {
+        
+        public string? Subject { get; set; }
+
+        public string? QueryBody { get; set; }
+
+        public IFormFile? File { get; set; }
+        
+    }
+}
