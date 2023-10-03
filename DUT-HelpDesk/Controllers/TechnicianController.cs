57a6c4ac778610f1dca05a0f64f9e3e6f50840e1
dylanscotthall <dylanscotthall@gmail.com>
2023-10-03 10:02:37 +0200

    technician lead minor bug fixes

--- DUT-HelpDesk/Controllers/TechnicianController.cs
+++ DUT-HelpDesk/Controllers/TechnicianController.cs
@@ -75,15 +75,7 @@ namespace DUT_HelpDesk.Controllers
         {
             StateManager.UnassignTicketTechnician((int)id, (int)techId);
             ViewBag.technician = StateManager.technician;
-            return View("TechnicianDashboard", StateManager.GetTechnicianTickets());
-        }
-
-        //When a ticket is unassigned from the technician ticket queue page, the technician is returned an updated view of the ticket queue.
-        public IActionResult UnassignTicketToTechnicianFromTicketQueue(int? id, int? techId)
-        {
-            StateManager.UnassignTicketTechnician((int)id, (int)techId);
-            ViewBag.technician = StateManager.technician;
-            return View("TechnicianTicketQueue", StateManager.GetAllTickets());
+            return View("TechnicianDashboard", StateManager.GetAllTickets());
         }
 
         public IActionResult TechnicianTicketQueueReport()
