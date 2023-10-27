a87381c2dede8519a5fbd3ff765719effee8d09a
EvanCode20 <evan.teague11@gmail.com>
2023-10-27 02:05:06 +0200

    reg + logout

--- DUT-HelpDesk/Controllers/LoginController.cs
+++ DUT-HelpDesk/Controllers/LoginController.cs
@@ -63,8 +63,66 @@ namespace DUT_HelpDesk.Controllers
             return View();  
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginViewModel model)
+        {
+            try
+            {
+                await auth.CreateUserWithEmailAndPasswordAsync(model.Email,model.Password);
+                
+                var fbAuthLink = await auth.SignInWithEmailAndPasswordAsync(model.Email,model.Password);
+                string token = fbAuthLink.FirebaseToken;
+                var fbUser = await auth.GetUserAsync(token);
+
+                DatabaseModels.User user = new DatabaseModels.User();
+                user.Email = model.Email;
+                user.FbId = fbUser.LocalId;
+                user.Type = "Student";
+                await db.Users.AddAsync(user);
+                await db.SaveChangesAsync();
+                if (token != null)
+                {
+                    HttpContext.Session.SetString("_UserToken", token);
+                    StateManager.token = token;
+                    HttpContext.Session.SetString("_UserID", fbAuthLink.User.LocalId);
+                    StateManager.user = StateManager.GetUserByFBId(fbAuthLink.User.LocalId);
+                    HttpContext.Session.SetString("_UserType", StateManager.user.Type);
+                    StateManager.email = fbAuthLink.User.Email;
+
+                    switch (StateManager.getUserType())
+                    {
+                        case "Student":
+                            return RedirectToAction("UserTicket", "User");
+                        case "Technician":
+                            StateManager.technician = StateManager.GetTechnician();
+                            return RedirectToAction("TechnicianTicketQueue", "Technician");
+                        case "TechnicianLead":
+                            StateManager.technician = StateManager.GetTechnician();
+                            return RedirectToAction("TechnicianTicketQueue", "Technician");
+                    }
+                }
+            }
+            catch (FirebaseAuthException ex)
+            {
+                //var firebaseEx = JsonSerializer.Deserialize<FirebaseError>(ex.ResponseData);
+                ModelState.AddModelError(String.Empty, "Invalid Credentials");
+                ViewBag.errors = "Login failed. Please check that your credentials are correct.";
+                return View(model);
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
 
 
-       
     }
 }
